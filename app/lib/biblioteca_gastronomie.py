cb3b83668ae9b6d891646d495efa75195bcbf161
Andrei Bădoi <andrei.badoi.12@gmail.com>
2026-05-07 18:30:02 +0300

    Delete app directory

--- app/lib/biblioteca_gastronomie.py
+++
@@ -1,8 +0,0 @@
-def provenienta_ramen():
-    return "Ramen este un preparat culinar originar din Japonia, deși are la bază influențe din bucătăria tradițională chineză."
-
-def ingrediente_ramen():
-    return "Tăiței, supă (shoyu, miso, tonkotsu), felii de carne de porc (chashu), ou marinat, ceapă verde și alge nori."
-
-def preparare_ramen():
-    return "Supa se fierbe lent, timp de mai multe ore. Tăițeii se fierb separat, se adaugă în bol peste supa fierbinte, iar la final se asamblează topping-urile."
