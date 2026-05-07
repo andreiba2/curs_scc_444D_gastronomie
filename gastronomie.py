ba57aab8200f7a397a45f08465a59c286b6054f4
Magda Nastase <nastasemagda923@gmail.com>
2026-05-07 14:12:41 +0000

    Actualizat rutele Flask cu nume, grupa si link-uri conform cerintelor

--- gastronomie.py
+++ gastronomie.py
@@ -3,21 +3,24 @@ from app.lib.biblioteca_gastronomie import culoare_carbonara, descriere_carbonar
 
 app = Flask(__name__)
 
+# Cerinta: Nume, Prenume, Grupa si Tema
+info = "<p><b>Student:</b> Nastase Magda</p><p><b>Grupa:</b> 444D</p><p><b>Tema:</b> Gastronomie</p>"
+
 @app.route('/gastronomie')
 def tema():
-    return "Tema: Gastronomie"
+    return f"{info} <h2>Pagina Temei</h2> <a href='/carbonara'>Vezi Preparatul</a>"
 
 @app.route('/carbonara')
 def element():
-    return "Preparat: Paste Carbonara"
+    return f"{info} <h2>Element ales: Paste Carbonara</h2> <a href='/culoare_carbonara'>Vezi Culoare</a> | <a href='/descriere_carbonara'>Vezi Descriere</a> | <a href='/gastronomie'>Inapoi</a>"
 
 @app.route('/culoare_carbonara')
 def culoare():
-    return culoare_carbonara()
+    return f"{info} <h3>Culoare:</h3> {culoare_carbonara()} <br><br> <a href='/carbonara'>Inapoi</a>"
 
 @app.route('/descriere_carbonara')
 def descriere():
-    return descriere_carbonara()
+    return f"{info} <h3>Descriere:</h3> {descriere_carbonara()} <br><br> <a href='/carbonara'>Inapoi</a>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
