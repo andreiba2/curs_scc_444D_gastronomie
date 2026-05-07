0e7a43e5a035fb3521cfc06a1eeb2ee4603cf6d8
Curca Andrei-Daniel <andreicurcadaniel@gmail.com>
2026-05-07 15:45:08 +0300

    Fortare adaugare app/lib/biblioteca_gastronomie.py pe git

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,5 @@
+def ingrediente_ramen():
+    return "Tăiței, supă (shoyu, miso, tonkotsu), felii de carne de porc (chashu), ou marinat, ceapă verde."
+
+def descriere_ramen():
+    return "Ramen este un fel de mâncare japonez foarte popular, constând într-o supă bogată, tăiței și diverse topping-uri."
