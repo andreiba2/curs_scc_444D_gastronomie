17ade63f29c45d285f898b14211941ee86274805
Magda Nastase <nastasemagda923@gmail.com>
2026-05-07 16:45:00 +0000

    Adaugat requirements si teste unitare pentru conformitate proiect

---
+++ test_gastronomie.py
@@ -0,0 +1,15 @@
+import unittest
+from app.lib.biblioteca_gastronomie import text_provenienta, text_ingrediente, text_preparare
+
+class TestGastronomie(unittest.TestCase):
+    def test_provenienta(self):
+        self.assertIn("Italia", text_provenienta())
+    
+    def test_ingrediente(self):
+        self.assertIn("Guanciale", text_ingrediente())
+
+    def test_preparare(self):
+        self.assertIn("emulsie", text_preparare())
+
+if __name__ == '__main__':
+    unittest.main()
