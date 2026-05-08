054b22037ca35d04d691a91a41dad1e6fc7940a1
Vladeigthteen <fraticiuvlad6@gmail.com>
2026-05-08 12:59:31 +0300

    Fortare adaugare folder lib blocat de gitignore

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,5 @@
+def ingrediente_bouyourdi() -> str:
+    return 'Bouyourdi contine: branza feta, rosii, ardei iute si ulei de masline, gatite la cuptor.'
+
+def descriere_bouyourdi() -> str:
+    return 'Bouyourdi este un aperitiv traditional grecesc picant si delicios.'
