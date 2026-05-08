30ed0730cb3070492f98a6ff63e487ac319a7d31
EnacheVictor <enachevictor887@gmail.com>
2026-05-08 16:19:43 +0000

    fixed biblioteca_gastronomie

--- app/lib/biblioteca_gastronomie.py
+++ app/lib/biblioteca_gastronomie.py
@@ -1,6 +1,6 @@
 def descriere_piftie():
-    return "Piftia este un preparat culinar tradițional, obținut prin fierberea>
+    return "Piftia este un preparat culinar tradițional, obținut prin fierberea"
 
 def ingrediente_piftie():
-    return "Ingrediente principale: carne de porc (picioare, urechi), mult ustu>
+    return "Ingrediente principale: carne de porc, usturoi, sare, piper boabe și apă"
 
