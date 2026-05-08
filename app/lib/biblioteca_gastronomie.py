0b188662d4cf4ab52bde88dfadbfb13f20c329de
catavoicu <catavoicu01@gmail.com>
2026-05-08 16:21:43 +0000

    fixed issues

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,5 @@
+def descriere_macarons():
+    return "Macarons sunt prăjituri franțuzești delicate, pe bază de bezea, crocante la exterior și moi la interior, unite de obicei printr-o cremă fină."
+
+def ingrediente_macarons():
+    return "Ingrediente principale: făină de migdale, zahăr pudră, albușuri de ou, zahăr tos, colorant alimentar și cremă pentru umplutură (ganache, cremă de unt sau gem)."
