14eb0740b2db46a36fd6be2968d3d30b6884d685
alee <alexandranitu0703@gmail.com>
2026-05-09 18:40:06 +0300

    Force add app folder to repository

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,8 @@
+def text_provenienta():
+    return "Clătitele americane provin din America de Nord și sunt cunoscute pentru textura lor pufoasă și gustul dulce, servite adesea cu sirop de arțar."
+
+def text_ingrediente():
+    return "Făină, lapte, ouă, praf de copt, zahăr, unt topit, sare, esență de vanilie, sirop de arțar."
+
+def text_preparare():
+    return "Se amestecă ingredientele uscate, apoi cele lichide, se combină ușor și se prăjesc clătitele în tigaie până devin aurii și pufoase."
