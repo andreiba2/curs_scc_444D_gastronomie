63789d2acca0adcd3ac114a1e2eef141a182fc2f
alexutz24 <alexandrubutunoi@yahoo.com>
2026-05-10 01:27:47 +0300

    Test6

--- tests/test_mici.py
+++ tests/test_mici.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
 import os
+
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.lib.biblioteca_gastronomie import reteta_mici, descriere_mici
