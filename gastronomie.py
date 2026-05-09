1fce7befdb02d8d4da2cad76bbb4139979586b46
alexutz24 <alexandrubutunoi@yahoo.com>
2026-05-10 01:38:16 +0300

    Test9

--- gastronomie.py
+++ gastronomie.py
@@ -3,21 +3,38 @@ from app.lib.biblioteca_gastronomie import reteta_mici, descriere_mici
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '''
+    <html>
+    <head><title>Gastronomie Romaneasca</title></head>
+    <body>
+        <h1>🍽️ Gastronomie Romaneasca</h1>
+        <ul>
+            <li><a href="/gastronomie">Gastronomie</a></li>
+            <li><a href="/mici">Mici</a></li>
+            <li><a href="/mici/reteta">Reteta Mici</a></li>
+            <li><a href="/mici/descriere">Descriere Mici</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
+
 @app.route('/gastronomie')
 def gastronomie():
-    return "Bine ati venit la sectiunea de Gastronomie romaneasca!"
+    return '<h1>🍽️ Gastronomie Romaneasca</h1><p>Bine ati venit!</p><a href="/">Inapoi</a>'
 
 @app.route('/mici')
 def mici():
-    return "Pagina dedicata micilor - preparatul traditional romanesc!"
+    return '<h1>🥩 Mici</h1><p>Preparatul traditional romanesc!</p><a href="/">Inapoi</a>'
 
 @app.route('/mici/reteta')
 def reteta():
-    return reteta_mici()
+    return f'<h1>📋 Reteta Mici</h1><p>{reteta_mici()}</p><a href="/">Inapoi</a>'
 
 @app.route('/mici/descriere')
 def descriere():
-    return descriere_mici()
+    return f'<h1>📖 Descriere Mici</h1><p>{descriere_mici()}</p><a href="/">Inapoi</a>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
