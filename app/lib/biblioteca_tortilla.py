f1727b3f49cb933ac46556df3b5f3c5aac0b2369
DanaSerbanescu <daniserb023@gmail.com>
2026-05-09 18:44:23 +0300

    Adauga biblioteca

---
+++ app/lib/biblioteca_tortilla.py
@@ -0,0 +1,8 @@
+def origine_tortilla():
+    return "Tortilla de patatas provine din Spania si este un preparat traditional al bucatariei spaniole."
+
+def ingrediente_tortilla():
+    return "Ingredientele principale sunt cartofii, ouale, ceapa, amestec de branzeturi, uleiul de masline, sarea si piperul."
+
+def preparare_tortilla():
+    return "Cartofii se prajesc usor, se amesteca cu ouale si ceapa, apoi se gateste compozitia in tigaie pe ambele parti."
