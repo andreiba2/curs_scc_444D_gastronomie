cf5ee9ec8cedc5bf710c14d1979dc1933d5afdda
mammoth <cristian.v.stanciulescu@gmail.com>
2026-05-09 18:55:22 +0300

    Updated test_ratatouille: change byte string back to string

--- tests/test_ratatouille.py
+++ tests/test_ratatouille.py
@@ -1,10 +1,10 @@
 from app.lib.biblioteca_gastronomie import descriere_ratatouille, origine_ratatouille
 
 def test_descriere_ratatouille():
-    assert b"rosii" in descriere_ratatouille()
-    assert b"ardei" in descriere_ratatouille()
-    assert b"Ratatouille" in descriere_ratatouille()
+    assert "rosii" in descriere_ratatouille()
+    assert "ardei" in descriere_ratatouille()
+    assert "Ratatouille" in descriere_ratatouille()
     
 def test_origine_ratatouille():
-    assert b"Proventa" in origine_ratatouille()
-    assert b"sudul Frantei" in origine_ratatouille()
+    assert "Proventa" in origine_ratatouille()
+    assert "sudul Frantei" in origine_ratatouille()
