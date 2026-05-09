042c8e7d3341fc7b215b85d0d7148270263077e0
Costea Mihai Daniel <mihai.daniel@student.upt.ro>
2026-05-09 18:03:26 +0300

    Finalizat dezvoltare Brasovence - Daniel Costea

--- gastronomie.py
+++ gastronomie.py
@@ -1,23 +1,23 @@
-from flask import Flask 
-from app.lib.biblioteca_gastronomie import descriere_brasovence, ingrediente_brasovence
+from flask import Flask, render_template
+from app.lib.biblioteca_gastronomie import descriere_brasovence, ingrediente_brasovence, tara_brasovence
 
 app = Flask(__name__)
 
 @app.route('/gastronomie')
 def tema():
-	return "Tema grupei 444D este Gastronomie!"
+    return render_template('index.html')
 
-@app.route('/brasovence')
-def element():
-	return "Preparatul meu: Brasovence."
-
-@app.route('/descriere_brasovence')
-def descriere():
-	return descriere_brasovence()
+@app.route('/tara_brasovence') # Aceasta este ruta pentru link-ul 1
+def tara():
+    return f"<h1>Locul de provenienta</h1><p>{tara_brasovence()}</p>"
 
 @app.route('/ingrediente_brasovence')
 def ingrediente():
-	return ingrediente_brasovence()
+    return f"<h1>Ingrediente</h1><p>{ingrediente_brasovence()}</p>"
+
+@app.route('/descriere_brasovence')
+def descriere():
+    return f"<h1>Modul de preparare</h1><p>{descriere_brasovence()}</p>"
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0' , port=5000)
+    app.run(host='0.0.0.0', port=5000)
