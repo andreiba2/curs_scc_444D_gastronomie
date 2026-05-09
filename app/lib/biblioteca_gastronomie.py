d645905a2d722f1b415d4345f9ff275c9a347cb0
budurmaria <budurmarua@gmail.com>
2026-05-09 15:22:18 +0000

    Adauga biblioteca gastronomiei

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,21 @@
+def origine():
+    return (
+        "Sushi este un preparat tradițional japonez, asociat cu Japonia. "
+        "În forma modernă, acesta folosește orez asezonat cu oțet și "
+        "ingrediente precum pește, fructe de mare, legume sau ou."
+    )
+
+
+def ingrediente():
+    return (
+        "Ingrediente uzuale: orez pentru sushi, oțet de orez, foi nori, pește sau fructe de mare, "
+        "legume, sos de soia, wasabi și ghimbir murat."
+    )
+
+
+def preparare():
+    return (
+        "Mod de preparare: se fierbe orezul, se asezonează cu oțet de orez, zahăr și sare, "
+        "apoi se adaugă ingredientele. Pentru maki, compoziția se rulează în nori cu ajutorul unei rogojini de bambus "
+        "și se taie în bucăți mici."
+    )
