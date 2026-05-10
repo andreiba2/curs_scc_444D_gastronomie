af6851cb7268c6c896aca29a81f1db539ba8925d
andreiba2 <andrei.badoi.12@gmail.com>
2026-05-11 01:25:11 +0300

    incarc folder-ul app ca sa mearga build-ul la Jenkins

---
+++ app/lib/biblioteca_gastronomie.py
@@ -0,0 +1,6 @@
+def ingrediente_paella():
+    return "Orez bomba, șofran, supă de pui/pește, fructe de mare, pui, iepure și legume."
+
+def preparare_paella():
+    return "Se călesc carnea și legumele într-o paellera. Se adaugă orezul și supa infuzată cu șofran. Se lasă la fiert fără a se amesteca, pentru a forma crusta (socarrat) la bază."
+
